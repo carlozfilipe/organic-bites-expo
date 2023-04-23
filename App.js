fd10be062830a508e88299e7013af85367a65cf1
Carlos Filipe <thecarlosfilipe@gmail.com>
2023-04-22 21:52:19 -0300

    feat: :sparkles: Add Basket component

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { SafeAreaView, StatusBar, View } from 'react-native';
+import { useFonts, Poppins_400Regular, Poppins_700Bold } from '@expo-google-fonts/poppins';
+import Basket from './src/screens/Basket';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  let [fontsLoaded] = useFonts({
+    'PoppinsRegular': Poppins_400Regular,
+    'PoppinsBold': Poppins_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return <View />
+  } else {
+  return (
+      <SafeAreaView>
+      <StatusBar />
+      <Basket />
+      </SafeAreaView>
+    );
+  }
+}
